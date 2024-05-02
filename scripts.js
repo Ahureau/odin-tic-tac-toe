616d8e236347b42b8fba7375d62605305a747826
Alex H <53427235+Ahureau@users.noreply.github.com>
2024-05-02 10:11:26 +0200

    All function work in console, now building game loop

--- scripts.js
+++ scripts.js
@@ -1,27 +1,25 @@
 // Creating tiles
 
 const gameTileProto = {
-    
-    getRow: function () { 
-        console.log(this.row);
+    reset: function() {
+        this.occup = "empty";
     },
-    
-    getCol: function () { 
-        console.log(this.col); 
-    },
-    
-    getOccup: function () { 
-        console.log(this.occup); 
+
+    isEmpty: function(){
+        return this.occup === "empty" ? true : false;
     },
     
     setOccup: function (newOccup) {
-        if (["p1", "p2", "empty"].includes(newOccup)) {
+        if (["X", "O"].includes(newOccup)) {
             this.occup = newOccup;
+
+            printBoard();
+
         } else {
-            console.log("The tile can be 'empty', 'p1', or 'p2'");
+            return false;
         }
     },
-}
+};
 
 const gameTileFactory = function(row, col){
     const tile = Object.create(gameTileProto);
@@ -57,7 +55,7 @@ const boardCreate = function(){
     }
 
     return board;
-}
+};
 
 const board = boardCreate();
 
@@ -83,7 +81,9 @@ function checkRows() {
         let allSame = rowTiles.every(tile => tile.occup === firstTileOccup);
 
         if (allSame && firstTileOccup !== "empty") {
-            console.log(`Row ${rowName} with ${firstTileOccup} wins!`);
+            return firstTileOccup; // changed this to pass winner
+        } else {
+            return false
         }
     });
 };
@@ -106,11 +106,92 @@ function checkCol() {
         let allSame = colTiles.every(tile => tile.occup === firstTileOccup);
 
         if (allSame && firstTileOccup !== "empty") {
-            console.log(`Row ${colName} with ${firstTileOccup} wins!`);
+            return firstTileOccup; // changed this to pass winner
+        } else {
+            return false
         }
     });
 };
 
 function checkDiag() {
-    if ()
+    if (((board.a1.occup === board.b2.occup && board.a1.occup === board.c3.occup) ||
+        (board.c1.occup === board.b2.occup && board.c1.occup === board.a3.occup)) &&
+        board.b2.occup !== "empty"){
+            return board.b2.occup;  // changed this to pass winner
+        } else {
+            return false;
+        };
+}
+
+function checkWin(){
+    checkRows();
+    checkCol();
+    checkDiag();
+};
+
+
+
+
+// Game run
+
+function printBoard() {
+    let boardString = '';
+    for (let row of ['a', 'b', 'c']) {
+        for (let col of [1, 2, 3]) {
+            const tile = board[`${row}${col}`];
+            boardString += tile.occup === 'empty' ? ' ' : tile.occup;
+            if (col < 3) {
+                boardString += ' || ';
+            }
+        }
+        if (row < 'c') {
+            boardString += '\n';
+        }
+    }
+    console.log(boardString);
+}
+
+function start(){
+    for (const tile in board) {
+        board[tile].reset();
+    }
+    printBoard();
+};
+
+function playerToggle(player){
+    return player === "X" ? "O" : "X";
+}
+
+function round(player){
+    let tile = prompt("Pick an empty tile (a1 to c3)");
+
+    if (Object.keys(board).includes(tile)){
+        if (board[tile].isEmpty()) {
+            board[tile].setOccup(player);
+        } else {
+            alert("Pick an empty tile");
+            round();
+        };
+    } else {
+        alert("pick a valid value")
+        round();
+    }
+}
+
+function roundLoop(){
+    let player = "X"
+
+    if (!checkWin()) {
+        round(player);
+        playerToggle(player);
+        roundLoop();
+    } else {
+        return checkWin()
+    }
+}
+
+function run() {
+    start();
+
+    
 }
